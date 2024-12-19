a13ac1c1f152fd5949a134987f8fd7b8ef8681c3
Victorio2001 <v.garciadelapinera@lyon.ort.asso.fr>
2024-12-19 01:17:01 +0100

    Ajout permissions Route  + Views

--- Controllers/Movie/MovieController.cs
+++ Controllers/Movie/MovieController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -37,6 +38,7 @@ namespace NomDuProjet.Controllers.Movie
 
         // GET: Movie
         [HttpGet]
+        [Authorize]
         public async Task<IActionResult> Index(string searchTerm, string movieGenre)
         {
             
